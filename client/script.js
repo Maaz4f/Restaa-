2bf17bacf3346bce2d9dd08bd2bad790459b777c
Maaz4f <114129442+Maaz4f@users.noreply.github.com>
2023-02-16 14:17:55 +0500

    Update script.js

--- client/script.js
+++ client/script.js
@@ -106,7 +106,7 @@ const typeText = (messageDiv, message) => {
 
 if (response.ok) {
   const data = await response.json();
-  const err = await response.text()
+  const err = await response.text();
   const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
 
   typeText(messageDiv, parsedData)
