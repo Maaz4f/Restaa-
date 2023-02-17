2d357c330676530a46bbac13c0b35c4f5950bf31
Maaz4f <114129442+Maaz4f@users.noreply.github.com>
2023-02-17 14:32:13 +0500

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,41 +1,38 @@
-import bot from './assets/bot.png'
-import user from './assets/user.svg'
-import "./style.css"
+import bot from './assets/bot.png';
+import user from './assets/user.svg';
+import "./style.css";
 
-const form = document.querySelector('form')
-const chatContainer = document.querySelector('#chat_container')
-let loadInterval
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+let loadInterval;
 
 function loader(element) {
     element.textContent = '';
-};
     loadInterval = setInterval(() => {
         // Update the text content of the loading indicator
         element.textContent += '.';
 
         // If the loading indicator has reached three dots, reset it
-     if (element.textContent === '....') {
-    element.textContent = '';
-    clearInterval(loadInterval)
+        if (element.textContent === '....') {
+            element.textContent = '';
+            clearInterval(loadInterval);
+        }
+    }, 500);
 }
 
-
 function typeText(element, text) {
-    let index = 0
+    let index = 0;
 
     let interval = setInterval(() => {
         if (index < text.length) {
-            element.innerHTML += text.charAt(index)
-            index++
+            element.innerHTML += text.charAt(index);
+            index++;
         } else {
-            clearInterval(interval)
+            clearInterval(interval);
         }
-    }, 20)
+    }, 20);
 }
 
-// generate unique ID for each message div of bot
-// necessary for typing text effect for that specific reply
-// without unique ID, typing text will work on every element
 function generateUniqueId() {
     const timestamp = Date.now();
     const randomNumber = Math.random();
@@ -47,7 +44,7 @@ function generateUniqueId() {
 function chatStripe(isAi, value, uniqueId) {
     return (
         `
-        <div class="wrapper ${isAi && 'ai'}">
+        <div class="wrapper ${isAi ? 'ai' : ''}">
             <div class="chat">
                 <div class="profile">
                     <img 
@@ -60,68 +57,55 @@ function chatStripe(isAi, value, uniqueId) {
         </div>
     `
     );
-};
-
-
-const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    const data = new FormData(form)
-
-    // user's chatstripe
-    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
-
-    // to clear the textarea input 
-    form.reset()
-
-    // bot's chatstripe
-    const uniqueId = generateUniqueId()
-    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
-
-    // to focus scroll to the bottom 
-    
-
-    // specific message div 
-    const messageDiv = document.getElementById(uniqueId)
-
-    // messageDiv.innerHTML = "..."
-    loader(messageDiv);
-
-try {
-    const response = await fetch('https://LegitimateIvoryLivedistro.maaz-gamergamer.repl.co/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            prompt: data.get('prompt')
-        })
-    })
-chatContainer.scrollTop = chatContainer.scrollHeight
-    if (response.ok) {
-        const data = await response.json();
-       
-        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
-
-        typeText(messageDiv, parsedData)
-    } else {
-        const err = await response.text()
-
-        messageDiv.innerHTML = "Something went wrong"
-        alert(err)
-    }
-} catch (error) {
-    messageDiv.innerHTML = "Network error. Please check your internet connection."
 }
 
-       
+const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    const data = new FormData(form);
 
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'), generateUniqueId());
 
-form.addEventListener('submit', handleSubmit)
+    form.reset();
+
+    const uniqueId = generateUniqueId();
+    chatContainer.innerHTML += chatStripe(true, '', uniqueId);
+
+    const messageDiv = document.getElementById(uniqueId);
+    loader(messageDiv);
+
+    try {
+        const response = await fetch('https://LegitimateIvoryLivedistro.maaz-gamergamer.repl.co/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                prompt: data.get('prompt')
+            })
+        });
+
+        chatContainer.scrollTop = chatContainer.scrollHeight;
+
+        if (response.ok) {
+            const data = await response.json();
+
+            const parsedData = data.bot.trim();
+            typeText(messageDiv, parsedData);
+        } else {
+            const err = await response.text();
+            messageDiv.innerHTML = 'Something went wrong';
+            alert(err);
+        }
+    } catch (error) {
+        messageDiv.innerHTML = 'Network error. Please check your internet connection.';
+    }
+};
+
+form.addEventListener('submit', handleSubmit);
+
 form.addEventListener('keyup', (e) => {
     if (e.keyCode === 13) {
-        handleSubmit(e)
+        handleSubmit(e);
     }
-})
-};
+});
